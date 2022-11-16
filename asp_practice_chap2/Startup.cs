f6c2346ec31b0676c627b2dedebdbbb938b43a9d
MarySweetRollStolen <marylostherself.com>
2022-11-16 09:58:01 +0200

    Add config data and EntityBase

--- asp_practice_chap2/Startup.cs
+++ asp_practice_chap2/Startup.cs
@@ -1,6 +1,8 @@
+using asp_practice_chap2.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,8 +15,11 @@ namespace asp_practice_chap2
     public class Startup
     {
 
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config());
             services.AddRazorPages();
             services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
 
