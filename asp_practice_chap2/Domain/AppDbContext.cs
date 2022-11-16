39ad191d5a478141aa15c38f068fb9176c830f23
MarySweetRollStolen <marylostherself.com>
2022-11-16 10:12:25 +0200

    Add entities

---
+++ asp_practice_chap2/Domain/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_practice_chap2.Domain
+{
+    public class AppDbContext
+    {
+    }
+}
